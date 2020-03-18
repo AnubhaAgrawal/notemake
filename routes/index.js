d9e5f7a0db73dec0edbb629e236e8aebe6f73a26
Anubha Agrawal <anubhakannnu@gmail.com>
2020-03-18 14:07:23 +0530

    make it able to edit and delete

--- routes/index.js
+++ routes/index.js
@@ -23,18 +23,61 @@ User.update({'_id': new mongodb.ObjectID(req.user._id)},
   }} },function(err, Todo){
     if(err) console.log(err);
     else{
-        console.log("Edited Item: " + req.user._id);
+        console.log("Inserted Item: " + req.user._id);
     }
 });
 
     res.redirect("/dashboard");
 });
 
-router.get('/destroy', function(req, res){
-    console.log("item Deleted " +req.user._id);
+router.get('/destroy/:id', function(req, res){
+    console.log("item Deleted " +req.params.id);
     var mongodb = require('mongodb');
+    var delItem ={_id: new mongodb.ObjectID(req.params.id)};
+    
+    User.updateOne({'_id': new mongodb.ObjectID(req.user._id)}, 
+    { $pull: {  'notes' :  delItem} }, { safe: true, multi:true }, function(err, Todo){
+        if(err) console.log(err);
+        else{
+            console.log("Deleted Item: " + delItem._id);
+        }
+    });
  
     res.redirect("/dashboard");
+});
+
+router.get('/dashboard', ensureAuthenticated, (req, res) => 
+res.render('dashboard', {
+    todoList: req.user
+    
+})
+);
+router.get('/edit/:id', ensureAuthenticated,  function(req, res){
+    var mongodb = require('mongodb');
+    var delItem ={_id: new mongodb.ObjectID(req.params.id)};
+    User.find({}, function(err, todoList){
+        if(err) console.log(err);
+        else{
+            res.render("todoEdit.ejs", {todoList: req.user, Todoid: delItem._id});
+        }
+    })
+router.post('/edit/:id', function(req, res){
+    console.log("item Edited");
+    var mongodb = require('mongodb');
+   
+    var r_name = req.body.items;
+    console.log("Edited Item: "+ r_name);
+    
+    User.updateOne({'_id': new mongodb.ObjectID(req.user.id), 'notes._id': new mongodb.ObjectID(req.params.id)}, 
+    { $set: {"notes.$.value": r_name} },function(err, Todo){
+        if(err) console.log(err);
+        else{
+            console.log("Edited Item: "+ req.params.id);
+        }
+    });
+
+    res.redirect("/dashboard");
+});
 });
 
 module.exports = router;
